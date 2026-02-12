f5b1c286f06ad3f4a42dee21c1c606c73c766ad6
koolkhan8586 <ammad@khanammad.com>
2026-02-12 12:39:38 +0500

    Update Staff.php

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -3,8 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Staff extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'employee_id',
+        'department',
+        'designation',
+        'salary',
+        'joining_date',
+    ];
+
+    protected $casts = [
+        'joining_date' => 'date',
+        'salary' => 'float',
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relationships
+    |--------------------------------------------------------------------------
+    */
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
