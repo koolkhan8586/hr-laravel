eca0e9524fd84681554ba09c3213fb77a1004c23
koolkhan8586 <ammad@khanammad.com>
2026-02-12 23:46:12 +0500

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,7 +10,10 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <img src="{{ asset('UOL-Green-V1.png') }}" 
+     alt="University of Lahore Logo"
+     class="h-14 w-auto">
+
                     </a>
                 </div>
 
