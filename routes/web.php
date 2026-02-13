2fe1795b54127229bc69182d803b995e2777ec5b
koolkhan8586 <ammad@khanammad.com>
2026-02-13 11:18:19 +0500

    Add route for myLoan in web.php

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/my-loan', [LoanController::class, 'myLoan'])->name('loan.my');
 
 });
 
