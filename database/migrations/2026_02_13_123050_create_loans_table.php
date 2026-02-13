21b00f27c5afed9550eaaae01ca72be3102f630a
koolkhan8586 <ammad@khanammad.com>
2026-02-13 17:31:47 +0500

    Update 2026_02_13_123050_create_loans_table.php

--- database/migrations/2026_02_13_123050_create_loans_table.php
+++ database/migrations/2026_02_13_123050_create_loans_table.php
@@ -1,25 +1,33 @@
-<?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('loans', function (Blueprint $table) {
+
             $table->id();
+
+            // Link to employee
+            $table->foreignId('user_id')
+                  ->constrained()
+                  ->cascadeOnDelete();
+
+            $table->decimal('amount', 12, 2);
+            $table->integer('installments');
+
+            $table->decimal('monthly_deduction', 12, 2)->default(0);
+            $table->decimal('remaining_balance', 12, 2)->default(0);
+
+            $table->enum('status', ['pending','approved','rejected'])
+                  ->default('pending');
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('loans');
