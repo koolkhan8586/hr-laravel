226cfd44bbc702c584b0ac61c37ec56803f34110
koolkhan8586 <ammad@khanammad.com>
2026-02-13 23:33:31 +0500

    Update LoanPayment.php

--- app/Models/LoanPayment.php
+++ app/Models/LoanPayment.php
@@ -1,10 +1,18 @@
-<?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class LoanPayment extends Model
 {
-    //
+    protected $fillable = [
+        'loan_id',
+        'salary_id',
+        'amount_paid',
+        'remaining_balance'
+    ];
+
+    public function loan()
+    {
+        return $this->belongsTo(Loan::class);
+    }
 }
