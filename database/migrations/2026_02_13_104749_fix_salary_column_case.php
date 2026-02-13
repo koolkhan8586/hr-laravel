ae0257d88654abe4c9a59392ed220f0800dc0bff
koolkhan8586 <ammad@khanammad.com>
2026-02-13 15:48:45 +0500

    Fix case of salary column names in migration

--- database/migrations/2026_02_13_104749_fix_salary_column_case.php
+++ database/migrations/2026_02_13_104749_fix_salary_column_case.php
@@ -1,24 +1,26 @@
-<?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        Schema::table('salaries', function (Blueprint $table) {
+
+            $table->renameColumn('Others', 'others');
+            $table->renameColumn('Eidi', 'eidi');
+
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::table('salaries', function (Blueprint $table) {
+
+            $table->renameColumn('others', 'Others');
+            $table->renameColumn('eidi', 'Eidi');
+
+        });
     }
 };
