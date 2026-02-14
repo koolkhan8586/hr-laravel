634df7eba96dcd6b21f6518bd5b2deafedce28a2
koolkhan8586 <ammad@khanammad.com>
2026-02-14 15:23:31 +0500

    Update 2026_02_14_102244_add_leave_balance_to_users_table.php

--- database/migrations/2026_02_14_102244_add_leave_balance_to_users_table.php
+++ database/migrations/2026_02_14_102244_add_leave_balance_to_users_table.php
@@ -4,25 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class AddLeaveBalanceToUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->decimal('leave_balance', 5, 2)
+                  ->default(0)
+                  ->after('role');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('leave_balance');
         });
     }
-};
+}
