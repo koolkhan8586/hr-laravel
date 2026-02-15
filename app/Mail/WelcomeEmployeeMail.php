2c7bc55a2826992d451ed39dc430e909f9d2d7d4
koolkhan8586 <ammad@khanammad.com>
2026-02-15 21:39:49 +0500

    Update WelcomeEmployeeMail.php

--- app/Mail/WelcomeEmployeeMail.php
+++ app/Mail/WelcomeEmployeeMail.php
@@ -2,52 +2,22 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
-use Illuminate\Queue\SerializesModels;
 
 class WelcomeEmployeeMail extends Mailable
 {
-    use Queueable, SerializesModels;
+    public $user;
+    public $password;
 
-    /**
-     * Create a new message instance.
-     */
-    public function __construct()
+    public function __construct($user, $password)
     {
-        //
+        $this->user = $user;
+        $this->password = $password;
     }
 
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
+    public function build()
     {
-        return new Envelope(
-            subject: 'Welcome Employee Mail',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this->subject('Welcome to HR Portal')
+            ->view('emails.welcome-employee');
     }
 }
