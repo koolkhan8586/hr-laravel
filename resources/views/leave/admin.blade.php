0ced76f97b6bbb39433b1f6b0d41d177b46e349c
koolkhan8586 <ammad@khanammad.com>
2026-02-15 19:26:56 +0500

    Update admin.blade.php

--- resources/views/leave/admin.blade.php
+++ resources/views/leave/admin.blade.php
@@ -17,6 +17,12 @@
    class="bg-blue-600 text-white px-4 py-2 rounded shadow">
     Add Leave
 </a>
+        <a href="{{ route('admin.leave.edit', $leave->id) }}"
+   class="bg-yellow-500 text-white px-3 py-1 rounded text-xs">
+    Edit
+</a>
+
+        
 
     </div>
 
