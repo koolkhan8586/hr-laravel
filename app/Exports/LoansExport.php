6d291e50562ffbc1bce4273433d17ea3ed99037f
koolkhan8586 <ammad@khanammad.com>
2026-02-15 15:25:17 +0500

    Update LoansExport.php

--- app/Exports/LoansExport.php
+++ app/Exports/LoansExport.php
@@ -1,12 +1,40 @@
+<?php
+
 namespace App\Exports;
 
 use App\Models\Loan;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class LoansExport implements FromCollection
+class LoansExport implements FromCollection, WithHeadings
 {
     public function collection()
     {
-        return Loan::with('user')->get();
+        return Loan::with('user')
+            ->get()
+            ->map(function ($loan) {
+                return [
+                    'Employee Name'     => $loan->user->name ?? '',
+                    'Amount'            => $loan->amount,
+                    'Installments'      => $loan->installments,
+                    'Monthly Deduction' => $loan->monthly_deduction,
+                    'Remaining Balance' => $loan->remaining_balance,
+                    'Status'            => $loan->status,
+                    'Created At'        => $loan->created_at->format('Y-m-d')
+                ];
+            });
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Employee Name',
+            'Amount',
+            'Installments',
+            'Monthly Deduction',
+            'Remaining Balance',
+            'Status',
+            'Created Date'
+        ];
     }
 }
