096b22d3a8f327fcd2e78766baa78bb1ebafcdbd
koolkhan8586 <ammad@khanammad.com>
2026-02-15 13:06:51 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -194,4 +194,13 @@ Route::middleware(['auth', 'admin'])
     Route::get('/salary/{id}', [SalaryController::class,'show'])
         ->name('admin.salary.show');
 
+        public function destroy($id)
+{
+    $salary = Salary::findOrFail($id);
+    $salary->delete();
+
+    return back()->with('success','Salary Deleted Successfully');
+}
+
+
 });
