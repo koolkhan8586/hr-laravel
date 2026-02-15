3f40ea5294dc08d9a50d26d5b9cfeecea5479627
koolkhan8586 <ammad@khanammad.com>
2026-02-16 01:42:32 +0500

    Update Salary.php

--- app/Models/Salary.php
+++ app/Models/Salary.php
@@ -7,24 +7,36 @@ use Illuminate\Database\Eloquent\Model;
 class Salary extends Model
 {
     protected $fillable = [
+
         'user_id',
         'month',
         'year',
+
+        // Earnings
         'basic_salary',
         'invigilation',
         't_payment',
-        'Others',
-        'Eidi',
+        'eidi',
         'increment',
+        'other_earnings',
+
+        // Deductions
         'extra_leaves',
         'income_tax',
         'loan_deduction',
         'insurance',
-        'others',
+        'other_deductions',
+
+        // Totals
         'gross_total',
         'total_deductions',
         'net_salary',
+
         'is_posted'
+    ];
+
+    protected $casts = [
+        'is_posted' => 'boolean',
     ];
 
     public function user()
