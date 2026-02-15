9c670ef565162f03fba8839104185e64d6b90e24
koolkhan8586 <ammad@khanammad.com>
2026-02-15 23:10:14 +0500

    Update SalarySampleExport.php

--- app/Exports/SalarySampleExport.php
+++ app/Exports/SalarySampleExport.php
@@ -3,28 +3,33 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class SalarySampleExport implements FromArray
+class SalarySampleExport implements FromArray, WithHeadings
 {
+    public function headings(): array
+    {
+        return [
+            'employee_email',
+            'month',
+            'year',
+            'basic_salary',
+            'allowance',
+            'deduction',
+        ];
+    }
+
     public function array(): array
     {
         return [
             [
-                'employee_email',
-                'month',
-                'year',
-                'basic_salary',
-                'allowance',
-                'deduction'
-            ],
-            [
-                'employee@email.com',
+                'zubair@email.com',
                 'February',
                 '2026',
-                '50000',
+                '70000',
                 '5000',
-                '2000'
-            ]
+                '2000',
+            ],
         ];
     }
 }
