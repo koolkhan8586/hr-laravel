3f3b07cb50a3bb7200510b7b297bcb75f25a0ea0
koolkhan8586 <ammad@khanammad.com>
2026-02-15 22:06:05 +0500

    Update StaffSampleExport.php

--- app/Exports/StaffSampleExport.php
+++ app/Exports/StaffSampleExport.php
@@ -2,8 +2,8 @@
 
 namespace App\Exports;
 
-use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class StaffSampleExport implements FromArray, WithHeadings
 {
