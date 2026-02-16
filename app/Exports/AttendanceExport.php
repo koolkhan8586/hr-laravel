bca5c4e0c454f2457ffe7d24fba32287c7082da6
koolkhan8586 <ammad@khanammad.com>
2026-02-16 14:37:53 +0500

    Update AttendanceExport.php

--- app/Exports/AttendanceExport.php
+++ app/Exports/AttendanceExport.php
@@ -2,15 +2,34 @@
 
 namespace App\Exports;
 
+use App\Models\Attendance;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Carbon\Carbon;
 
 class AttendanceExport implements FromCollection
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
+    protected $month;
+
+    public function __construct($month)
+    {
+        $this->month = $month;
+    }
+
     public function collection()
     {
-        //
+        $month = Carbon::parse($this->month);
+
+        return Attendance::with('user')
+            ->whereMonth('clock_in',$month->month)
+            ->whereYear('clock_in',$month->year)
+            ->get()
+            ->map(function($r){
+                return [
+                    'Employee'=>$r->user->name,
+                    'Date'=>$r->clock_in,
+                    'Hours'=>$r->total_hours,
+                    'Status'=>$r->status
+                ];
+            });
     }
 }
