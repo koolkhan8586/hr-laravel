e5f9a02689267de773d056b79b9b4781b691492a
koolkhan8586 <ammad@khanammad.com>
2026-02-16 20:24:35 +0500

    Update kernel.php

--- app/Console/Commands/kernel.php
+++ app/Console/Commands/kernel.php
@@ -3,3 +3,7 @@ protected function schedule(Schedule $schedule)
     $schedule->command('attendance:mark-absent')
              ->dailyAt('18:00');
 }
+
+$schedule->command('attendance:auto-clockout')
+         ->dailyAt('21:00')
+         ->timezone('Asia/Karachi');
