159123fe8b4a772f8ffae84c4d63f00d76d33eb5
koolkhan8586 <ammad@khanammad.com>
2026-02-17 14:20:13 +0500

    Update 2026_02_17_141838_create_leave_balances_table.php

--- database/migrations/2026_02_17_141838_create_leave_balances_table.php
+++ database/migrations/2026_02_17_141838_create_leave_balances_table.php
@@ -4,24 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('leave_balances', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('user_id')
+                  ->constrained()
+                  ->cascadeOnDelete();
+
+            $table->integer('opening_balance')->default(0);
+            $table->integer('used_leaves')->default(0);
+            $table->integer('remaining_leaves')->default(0);
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('leave_balances');
     }
 };
+
