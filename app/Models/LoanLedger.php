e5e390403148cc539d85bd6a8a43a80f2b5b5dbc
koolkhan8586 <ammad@khanammad.com>
2026-02-17 09:39:52 +0500

    Update LoanLedger.php

--- app/Models/LoanLedger.php
+++ app/Models/LoanLedger.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class LoanLedger extends Model
 {
-    //
+    protected $fillable = [
+        'loan_id',
+        'amount',
+        'type',
+        'remarks'
+    ];
+
+    public function loan()
+    {
+        return $this->belongsTo(Loan::class);
+    }
 }
