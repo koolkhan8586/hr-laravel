2ac830c781480b816aa6bc07867e2ffffb602fbf
koolkhan8586 <ammad@khanammad.com>
2026-02-17 15:17:14 +0500

    Update LeaveBalance.php

--- app/Models/LeaveBalance.php
+++ app/Models/LeaveBalance.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class LeaveBalance extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'opening_balance',
+        'used_leaves',
+        'remaining_leaves'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
