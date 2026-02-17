1e2ad46359f89c65be40f213efc888e1bb034cde
koolkhan8586 <ammad@khanammad.com>
2026-02-17 09:34:06 +0500

    Update 2026_02_17_093326_create_loan_ledgers_table.php

--- database/migrations/2026_02_17_093326_create_loan_ledgers_table.php
+++ database/migrations/2026_02_17_093326_create_loan_ledgers_table.php
@@ -1,25 +1,24 @@
-<?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('loan_ledgers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('loan_id')
+                  ->constrained()
+                  ->cascadeOnDelete();
+
+            $table->decimal('amount', 12,2);
+            $table->string('type'); // opening, deduction, adjustment
+            $table->text('remarks')->nullable();
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('loan_ledgers');
