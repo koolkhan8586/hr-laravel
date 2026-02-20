24004273b0d9de8f60d6e7c47fd3eb84b0dd4289
koolkhan8586 <ammad@khanammad.com>
2026-02-21 00:31:41 +0500

    Update SalariesExport.php

--- app/Exports/SalariesExport.php
+++ app/Exports/SalariesExport.php
@@ -12,7 +12,7 @@ class SalariesExport implements FromCollection, WithHeadings
     {
         return Salary::with('user')->get()->map(function ($salary) {
             return [
-                'Employee ID'     => $salary->employee_id->id ?? '',
+                'employee_id'     => $salary->employee_id->id ?? '',
                 'Employee' => $salary->user->name ?? '',
                 'Month' => $salary->month,
                 'Year' => $salary->year,
