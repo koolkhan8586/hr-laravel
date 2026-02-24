84d382d39cb74fd5171277ad3e5e2e33f832fc8f
koolkhan8586 <ammad@khanammad.com>
2026-02-24 14:42:15 +0500

    Update 2026_02_24_143546_add_employee_code_to_users_table.php

--- database/migrations/2026_02_24_143546_add_employee_code_to_users_table.php
+++ database/migrations/2026_02_24_143546_add_employee_code_to_users_table.php
@@ -9,16 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
-{
-    Schema::table('users', function (Blueprint $table) {
-        $table->string('employee_code')->unique()->nullable()->after('id');
-    });
-}
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('employee_code')->unique()->nullable()->after('id');
+        });
+    }
 
-public function down()
-{
-    Schema::table('users', function (Blueprint $table) {
-        $table->dropColumn('employee_code');
-    });
-}
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('employee_code');
+        });
+    }
+};
