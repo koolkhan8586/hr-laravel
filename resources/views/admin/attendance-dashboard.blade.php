427687dae8e311b0a370040c6bdca847dff14af2
koolkhan8586 <ammad@khanammad.com>
2026-03-04 17:53:52 +0500

    Update attendance-dashboard.blade.php

--- resources/views/admin/attendance-dashboard.blade.php
+++ resources/views/admin/attendance-dashboard.blade.php
@@ -64,22 +64,31 @@
 
 @php
 $minutes = \Carbon\Carbon::parse($attendance->clock_in)
-->diffInMinutes(now());
+    ->diffInMinutes(now('Asia/Karachi'));
 
 $hours = floor($minutes / 60);
 $mins = $minutes % 60;
 @endphp
 
 <tr>
-<td class="p-2 border">{{ $attendance->user->name }}</td>
-<td class="p-2 border">{{ $attendance->clock_in }}</td>
-<td class="p-2 border">{{ $hours }}h {{ $mins }}m</td>
+<td class="p-2 border">
+{{ $attendance->user->name ?? 'Unknown' }}
+</td>
+
+<td class="p-2 border">
+{{ \Carbon\Carbon::parse($attendance->clock_in)->format('H:i:s') }}
+</td>
+
+<td class="p-2 border">
+{{ $hours }}h {{ $mins }}m
+</td>
 </tr>
 
 @endforeach
 
 </tbody>
 </table>
+
 <script>
 setInterval(function(){
     location.reload();
