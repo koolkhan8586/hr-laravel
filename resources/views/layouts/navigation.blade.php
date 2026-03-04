97efc13d962c9439b6168e987ffcc6b7f15f799a
koolkhan8586 <ammad@khanammad.com>
2026-03-04 22:47:35 +0500

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -91,6 +91,9 @@
                             <a href="{{ route('admin.attendance.dashboard') }}" class="block px-4 py-2 hover:bg-gray-100">
     Attendance Dashboard
 </a>
+                            <a href="{{ route('admin.attendance.calendar') }}">
+Attendance Calendar
+</a>
 
                             <a href="{{ route('admin.leave.index') }}"
                                class="block px-4 py-2 hover:bg-gray-100">
