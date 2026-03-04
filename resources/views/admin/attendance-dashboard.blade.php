e501be708d1f9e64f2e4aa817007538920a7321c
koolkhan8586 <ammad@khanammad.com>
2026-03-04 15:25:06 +0500

    Update attendance-dashboard.blade.php

--- resources/views/admin/attendance-dashboard.blade.php
+++ resources/views/admin/attendance-dashboard.blade.php
@@ -1,44 +1,44 @@
 <x-app-layout>
 
-<div class="grid grid-cols-6 gap-6 mb-8">
+<div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-6 mb-8">
 
 <a href="{{ route('admin.attendance.list','present') }}">
-<div class="bg-green-100 p-6 rounded-lg shadow text-center hover:shadow-lg transition">
+<div class="bg-green-100 p-5 rounded-lg shadow text-center">
 <h3 class="text-lg font-semibold text-green-700">Present</h3>
 <p class="text-3xl font-bold">{{ $present }}</p>
 </div>
 </a>
 
 <a href="{{ route('admin.attendance.list','late') }}">
-<div class="bg-yellow-100 p-6 rounded-lg shadow text-center hover:shadow-lg transition">
+<div class="bg-yellow-100 p-5 rounded-lg shadow text-center">
 <h3 class="text-lg font-semibold text-yellow-700">Late</h3>
 <p class="text-3xl font-bold">{{ $late }}</p>
 </div>
 </a>
 
 <a href="{{ route('admin.attendance.list','halfday') }}">
-<div class="bg-purple-100 p-6 rounded-lg shadow text-center hover:shadow-lg transition">
+<div class="bg-purple-100 p-5 rounded-lg shadow text-center">
 <h3 class="text-lg font-semibold text-purple-700">Half Day</h3>
 <p class="text-3xl font-bold">{{ $halfday }}</p>
 </div>
 </a>
 
 <a href="{{ route('admin.attendance.list','leave') }}">
-<div class="bg-blue-100 p-6 rounded-lg shadow text-center hover:shadow-lg transition">
+<div class="bg-blue-100 p-5 rounded-lg shadow text-center">
 <h3 class="text-lg font-semibold text-blue-700">Leave</h3>
 <p class="text-3xl font-bold">{{ $leave }}</p>
 </div>
 </a>
 
 <a href="{{ route('admin.attendance.list','absent') }}">
-<div class="bg-red-100 p-6 rounded-lg shadow text-center hover:shadow-lg transition">
+<div class="bg-red-100 p-5 rounded-lg shadow text-center">
 <h3 class="text-lg font-semibold text-red-700">Absent</h3>
 <p class="text-3xl font-bold">{{ $absent }}</p>
 </div>
 </a>
 
 <a href="{{ route('admin.attendance.list','working') }}">
-<div class="bg-indigo-100 p-6 rounded-lg shadow text-center hover:shadow-lg transition">
+<div class="bg-indigo-100 p-5 rounded-lg shadow text-center">
 <h3 class="text-lg font-semibold text-indigo-700">Working</h3>
 <p class="text-3xl font-bold">{{ $working->count() }}</p>
 </div>
