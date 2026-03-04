bc74670f2d83302e4e4700817c5d7175e2832904
koolkhan8586 <ammad@khanammad.com>
2026-03-04 21:43:46 +0500

    Update Attendance.php

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -7,35 +7,48 @@ use Carbon\Carbon;
 
 class Attendance extends Model
 {
-    protected $fillable = [
-        'user_id',
-        'date',
-        'clock_in',
-        'clock_out',
-        'clock_in_latitude',
-        'clock_in_longitude',
-        'clock_out_latitude',
-        'clock_out_longitude',
-        'status',
-        'total_hours'
-    ];
 
-    /*
-    |--------------------------------------------------------------------------
-    | Automatically set date when clock_in is saved
-    |--------------------------------------------------------------------------
-    */
+protected $fillable = [
+    'user_id',
+    'date',
+    'clock_in',
+    'clock_out',
+    'clock_in_latitude',
+    'clock_in_longitude',
+    'clock_out_latitude',
+    'clock_out_longitude',
+    'status',
+    'total_hours'
+];
 
-    protected static function boot()
-    {
-        parent::boot();
+/*
+|--------------------------------------------------------------------------
+| User Relationship
+|--------------------------------------------------------------------------
+*/
 
-        static::creating(function ($attendance) {
+public function user()
+{
+    return $this->belongsTo(\App\Models\User::class);
+}
 
-            if (!$attendance->date && $attendance->clock_in) {
-                $attendance->date = Carbon::parse($attendance->clock_in)->toDateString();
-            }
+/*
+|--------------------------------------------------------------------------
+| Auto set date
+|--------------------------------------------------------------------------
+*/
 
-        });
-    }
+protected static function boot()
+{
+    parent::boot();
+
+    static::creating(function ($attendance) {
+
+        if (!$attendance->date && $attendance->clock_in) {
+            $attendance->date = Carbon::parse($attendance->clock_in)->toDateString();
+        }
+
+    });
+}
+
 }
