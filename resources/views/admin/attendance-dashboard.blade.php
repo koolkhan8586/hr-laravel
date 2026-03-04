111e80ef74529b4d830948411922bdbcd76a1cf5
koolkhan8586 <ammad@khanammad.com>
2026-03-04 17:11:42 +0500

    Update attendance-dashboard.blade.php

--- resources/views/admin/attendance-dashboard.blade.php
+++ resources/views/admin/attendance-dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-<div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-6 mb-8">
+<div class="grid grid-cols-2 md:grid-cols-6 gap-4 mb-8">
 
 <a href="{{ route('admin.attendance.list','present') }}">
 <div class="bg-green-100 p-5 rounded-lg shadow text-center">
