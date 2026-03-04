5f88d34472b13fcc72fbe470bd53c24f02a34cf9
root <root@vmi2860052.contaboserver.net>
2026-03-04 06:59:20 +0500

    Updated files on server

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,35 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+/*
+|--------------------------------------------------------------------------
+| Console Routes
+|--------------------------------------------------------------------------
+*/
 
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+    $this->comment('Keep pushing forward!');
 })->purpose('Display an inspiring quote');
+
+/*
+|--------------------------------------------------------------------------
+| Scheduled Commands
+|--------------------------------------------------------------------------
+*/
+
+Schedule::command('attendance:mark-absent')
+    ->dailyAt('11:45')
+    ->timezone('Asia/Karachi')
+    ->withoutOverlapping();
+
+Schedule::command('attendance:auto-clockout')
+    ->dailyAt('20:30')
+    ->timezone('Asia/Karachi')
+    ->withoutOverlapping();
+
+Schedule::command('app:send-daily-attendance-summary')
+    ->dailyAt('21:00')
+    ->timezone('Asia/Karachi')
+    ->withoutOverlapping();
