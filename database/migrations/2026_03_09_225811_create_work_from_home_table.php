7ab33ebd4845448810d3211274a150209286c306
koolkhan8586 <ammad@khanammad.com>
2026-03-09 23:50:44 +0500

    Update 2026_03_09_225811_create_work_from_home_table.php

--- database/migrations/2026_03_09_225811_create_work_from_home_table.php
+++ database/migrations/2026_03_09_225811_create_work_from_home_table.php
@@ -6,35 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('work_from_home', function (Blueprint $table) {
 
             $table->id();
 
-            // employee who requested WFH
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
 
-            // date of WFH
-            $table->date('date');
+            $table->date('start_date');
+            $table->date('end_date');
 
-            // reason for WFH
-            $table->text('reason')->nullable();
-
-            // approval status
-            $table->string('status')->default('pending');
-            // pending | approved | rejected
+            $table->string('reason')->nullable();
 
             $table->timestamps();
+
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('work_from_home');
