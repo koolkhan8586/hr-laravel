0f38794e9d511a273f2c62b26c3a8e702ae9302b
koolkhan8586 <ammad@khanammad.com>
2026-03-10 00:27:02 +0500

    Update WorkFromHome.php

--- app/Models/WorkFromHome.php
+++ app/Models/WorkFromHome.php
@@ -2,23 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class WorkFromHome extends Model
 {
+    use HasFactory;
 
-protected $table = "work_from_home";
+    protected $table = 'work_from_home';
 
-protected $fillable = [
-'user_id',
-'start_date',
-'end_date',
-'reason'
-];
+    protected $fillable = [
+        'user_id',
+        'start_date',
+        'end_date',
+        'reason'
+    ];
 
-}
-
-public function user()
-{
-return $this->belongsTo(User::class);
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
