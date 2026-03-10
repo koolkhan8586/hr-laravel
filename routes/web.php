fa2def84c4e7833ba4435262e7297b4a191a8a40
koolkhan8586 <ammad@khanammad.com>
2026-03-10 11:46:38 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::get('/dashboard', function () {
 
 });
 
-require **DIR**.'/auth.php';
+require DIR.'/auth.php';
 
  /* |                                                                        |
 | -------------------------------------------------------------------------- |
