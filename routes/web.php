54537c567fa10768fc9e7f33ea48839e50ebf4cf
koolkhan8586 <ammad@khanammad.com>
2026-03-16 11:34:35 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -264,14 +264,14 @@ Route::get('/holidays',[HolidayController::class,'index'])->name('holidays.index
 
 Route::post('/holidays',[HolidayController::class,'store'])->name('holidays.store');
 
-Route::get('/admin/holidays/{id}/edit',
-[HolidayController::class,'edit'])->name('admin.holidays.edit');
+Route::get('/holidays/{id}/edit',
+[HolidayController::class,'edit'])->name('holidays.edit');
 
-Route::put('/admin/holidays/{id}',
-[HolidayController::class,'update'])->name('admin.holidays.update');
+Route::put('/holidays/{id}',
+[HolidayController::class,'update'])->name('holidays.update');
 
-Route::delete('/admin/holidays/{id}',
-[HolidayController::class,'destroy'])->name('admin.holidays.delete');
+Route::delete('/holidays/{id}',
+[HolidayController::class,'destroy'])->name('holidays.delete');
 /*
 |--------------------------------------------------------------------------
 | Work From Home Management
