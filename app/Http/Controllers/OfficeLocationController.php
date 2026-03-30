f0aa484046e8ad27a7e7f615de260a9cf0d939ad
koolkhan8586 <ammad@khanammad.com>
2026-03-30 13:53:29 +0500

    Add CRUD methods to OfficeLocationController
    Implement CRUD operations for office locations.

--- app/Http/Controllers/OfficeLocationController.php
+++ app/Http/Controllers/OfficeLocationController.php
@@ -6,5 +6,37 @@ use Illuminate\Http\Request;
 
 class OfficeLocationController extends Controller
 {
-    //
+    public function index()
+    {
+        $locations = OfficeLocation::all();
+        return view('admin.locations.index', compact('locations'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'latitude' => 'required',
+            'longitude' => 'required',
+            'radius' => 'required'
+        ]);
+
+        OfficeLocation::create($request->all());
+
+        return back()->with('success', 'Location added');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $location = OfficeLocation::findOrFail($id);
+        $location->update($request->all());
+
+        return back()->with('success', 'Location updated');
+    }
+
+    public function destroy($id)
+    {
+        OfficeLocation::destroy($id);
+        return back()->with('success', 'Deleted');
+    }
 }
