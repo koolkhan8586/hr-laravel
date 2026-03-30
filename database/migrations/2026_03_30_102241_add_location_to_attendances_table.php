6c5c2aaca8b99d6041ea9d2129da983f5b3c18d9
koolkhan8586 <ammad@khanammad.com>
2026-03-30 11:34:32 +0500

    Update 2026_03_30_102241_add_location_to_attendances_table.php

--- database/migrations/2026_03_30_102241_add_location_to_attendances_table.php
+++ database/migrations/2026_03_30_102241_add_location_to_attendances_table.php
@@ -10,21 +10,43 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::table('attendances', function (Blueprint $table) {
-    $table->decimal('latitude', 10, 7)->nullable();
-    $table->decimal('longitude', 10, 7)->nullable();
-    $table->string('location_status')->nullable(); // inside / override
-});
-    }
+{
+    Schema::table('attendances', function (Blueprint $table) {
+
+        if (!Schema::hasColumn('attendances', 'latitude')) {
+            $table->decimal('latitude', 10, 7)->nullable();
+        }
+
+        if (!Schema::hasColumn('attendances', 'longitude')) {
+            $table->decimal('longitude', 10, 7)->nullable();
+        }
+
+        if (!Schema::hasColumn('attendances', 'location_status')) {
+            $table->string('location_status')->nullable();
+        }
+
+    });
+}
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
-    {
-        Schema::table('attendances', function (Blueprint $table) {
-            //
-        });
-    }
+{
+    Schema::table('attendances', function (Blueprint $table) {
+
+        if (Schema::hasColumn('attendances', 'latitude')) {
+            $table->dropColumn('latitude');
+        }
+
+        if (Schema::hasColumn('attendances', 'longitude')) {
+            $table->dropColumn('longitude');
+        }
+
+        if (Schema::hasColumn('attendances', 'location_status')) {
+            $table->dropColumn('location_status');
+        }
+
+    });
+}
 };
